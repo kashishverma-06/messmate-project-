976fc2167d2c12edb4f1d3b60680e8e20e96b569
kashishverma-06 <kashishverma1806@gmail.com>
2026-07-12 22:23:20 +0530

    ui updated

--- messmate-frontend/src/services/api.js
+++ messmate-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: 'https://messmate-project.onrender.com', 
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
